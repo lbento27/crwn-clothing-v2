30ecdd592e89b3f4b3e94669040cd2756d15b4d1
Ludgero Bento <lbento525@gmail.com>
2022-11-24 01:38:55 +0000

    update our application to use react router, creact home and navigation component and style our navigation component

--- src/App.js
+++ src/App.js
@@ -1,37 +1,18 @@
 //import React from 'react';//i do this to have react snippets in vscode
+import { Routes, Route } from 'react-router-dom';
 
-import Directory from './components/directory/directory.component';
+import Navigation from './routes/navigation/navigation.component';
+import Home from './routes/home/home.component';
 
 const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: 'hats',
-      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-    },
-    {
-      id: 2,
-      title: 'jackets',
-      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-    },
-    {
-      id: 3,
-      title: 'sneakers',
-      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-    },
-    {
-      id: 4,
-      title: 'womens',
-      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-    },
-    {
-      id: 5,
-      title: 'mens',
-      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-    },
-  ];
-
-  return <Directory categories={categories} />;
+  //index makes the 'default' component to render with the parent in the parent path
+  return (
+    <Routes>
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,26 @@
+import { Fragment } from 'react'; // Fragment it replace a top level parent that react needs but its nos a html element
+import { Outlet, Link } from 'react-router-dom'; //outlet its where we want the Route children to render
+
+import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
+
+import './navigation.styles.scss';
+
+const Navigation = () => {
+  return (
+    <Fragment>
+      <div className='navigation'>
+        <Link className='logo-container' to='/'>
+          <CrwnLogo className='logo' />
+        </Link>
+        <div className='nav-links-container'>
+          <Link className='nav-link' to='/shop'>
+            SHOP
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
+
+export default Navigation;
