8e1e14be26d8f4f3662a84e9f0f546b98fe36b0f
Ludgero Bento <lbento525@gmail.com>
2022-12-06 00:45:03 +0000

    link directory item to is respectives routes

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,18 +1,20 @@
 //import React from "react";//i do this to have react snippets in vscode
 import { BackgroundImage, Body, DirectoryItemContainer } from './directory-item.styles';
 
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const DirectoryItem = ({ category }) => {
-  const { imageUrl, title } = category;
+  const { imageUrl, title, route } = category;
+
+  const navigate = useNavigate();
+  const onNavigateHandler = () => navigate(route);
+
   return (
-    <DirectoryItemContainer>
+    <DirectoryItemContainer onClick={onNavigateHandler}>
       <BackgroundImage imageUrl={imageUrl} />
       <Body>
         <h2>{title}</h2>
-        <Link to={`/shop/${title}`}>
-          <p>Shop Now</p>
-        </Link>
+        <p>Shop Now</p>
       </Body>
     </DirectoryItemContainer>
   );
