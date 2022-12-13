b795ad299b27208152e960e401fbbf039ff2c63a
Ludgero Bento <lbento525@gmail.com>
2022-12-13 16:02:20 +0000

    exemple of creacting our own middleware, logger

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,26 @@
 import { compose, createStore, applyMiddleware } from 'redux';
-import logger from 'redux-logger';
+//import logger from 'redux-logger';
 
 import { rootReducer } from './root-reducer';
+//creating our middleware, see curring
+
+const loggerMiddleware = (store) => (next) => (action) => {
+  if (!action.type) {
+    return next(action);
+  }
+
+  console.log('type: ', action.type);
+  console.log('payload: ', action.payload);
+  console.log('currentState: ', store.getState());
+
+  next(action);
+
+  console.log('nextState: ', store.getState());
+};
 
 //logger
 //middleware its a func that when we dispatch a action before the action hit the reducers, hits the middleware first
-const middleware = [logger];
+const middleware = [loggerMiddleware];
 const composedEnhancers = compose(applyMiddleware(...middleware));
 
 //root-reducer
