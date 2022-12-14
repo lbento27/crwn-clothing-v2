4e550adcd625ebab66b49574a8a2dbdff0deaa4d
Ludgero Bento <lbento525@gmail.com>
2022-12-14 00:46:13 +0000

    added support on our app for the redux devTools extension in chrome

--- src/store/store.js
+++ src/store/store.js
@@ -20,8 +20,15 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 //logger
 //middleware its a func that when we dispatch a action before the action hit the reducers, hits the middleware first
-const middleware = [process.env.NODE_ENV === 'development' && logger].filter(Boolean); //.filter(Boolean) this makes that if the statement is false will get remove, ex [false] goes to empty [] if true will keep obj logger [logger]
-const composedEnhancers = compose(applyMiddleware(...middleware));
+const middleware = [process.env.NODE_ENV !== 'production' && logger].filter(Boolean); //.filter(Boolean) this makes that if the statement is false will get remove, ex [false] goes to empty [] if true will keep obj logger [logger]
+
+//to use with redux devtools extension in chrome
+const composedEnhancer =
+  (process.env.NODE_ENV !== 'production' && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+//if devtools extension present use it if not use regular compose from redux
+
+//const composedEnhancers = compose(applyMiddleware(...middleware));
+const composedEnhancers = composedEnhancer(applyMiddleware(...middleware));
 
 //root-reducer
 
