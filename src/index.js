b3fa3c9ed0fc7ed04af168550c04e4f0103fd222
Ludgero Bento <lbento525@gmail.com>
2023-02-03 22:45:47 +0000

    updating app to PWA

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+//import reportWebVitals from './reportWebVitals';
 
 import { BrowserRouter } from 'react-router-dom';
 
@@ -15,6 +15,8 @@ import { store, persistor } from './store/store';
 
 import { Elements } from '@stripe/react-stripe-js';
 import { stripePromise } from './utils/stripe/stripe.utils';
+
+import * as serviceWorkerRegistration from './serviceWorkerRegistration.ts';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -34,4 +36,6 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//reportWebVitals();
+
+serviceWorkerRegistration.register();
